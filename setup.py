6c5a527eee3947079e8f8f78ff200df0ba685166
coder-aastha <aasthadahal312@gmail.com>
2025-06-30 08:59:54 +0545

    Initial project setup and dependencies

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Aastha Dahal',
+    author_email= 'aasthadahal312@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
